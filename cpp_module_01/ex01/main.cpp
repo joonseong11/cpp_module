6b20c2768097644715ba5ee69df5d5020039bd8f
joonseong11 <jwjsjwnsstjd@gmail.com>
2022-12-25 12:25:47 +0900

    auto

--- cpp_module_01/ex01/main.cpp
+++ cpp_module_01/ex01/main.cpp
@@ -3,10 +3,10 @@
 int main()
 {
 	Zombie *zombies = zombieHorde(3, "horde1");
-	zombies->debug(zombies[0]);
-	zombies->debug(zombies[1]);
-	zombies->debug(zombies[2]);
-	zombies->debug(zombies[3]);
+	// zombies->debug(zombies[0]);
+	// zombies->debug(zombies[1]);
+	// zombies->debug(zombies[2]);
+	// zombies->debug(zombies[3]);
 	// zombies[0].announce();
 	delete[] zombies;
-}
+}gi
