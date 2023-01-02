187ecddbd6163b18c063b9ab39e4b7fde3504da4
junseong jeon <jwjsjwnsstjd@gmail.com>
2023-01-02 15:35:00 +0900

    auto

--- cpp_module_02/ex01/Fixed.hpp
+++ cpp_module_02/ex01/Fixed.hpp
@@ -64,18 +64,20 @@ Fixed::~Fixed(void)
 
 Fixed::Fixed(const int i)
 {
-	fixedPointNum = i;
+	std::cout << "Int constructor called" << std::endl;
+	fixedPointNum = i * (1 << fractionBit);
 }
 Fixed::Fixed(const float fl)
 {
+	std::cout << "Float constructor called" << std::endl;
 	fixedPointNum = roundf(fl * (1 << fractionBit));
 }
 float Fixed::toFloat(void) const
 {
-	return fixedPointNum / (1 << fractionBit);
+	return (float)fixedPointNum / (float)(1 << fractionBit);
 }
 int Fixed::toInt(void) const
 {
-	return fixedPointNum * (1 >> fractionBit);
+	return fixedPointNum >> fractionBit;
 }
 #endif
