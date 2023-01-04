424690270c32701ddb94c4885aaee7b7f2a906a5
joonseong11 <jwjsjwnsstjd@gmail.com>
2023-01-04 13:16:27 +0900

    auto

---
+++ practice/inherit/downcasting.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class Base {
+  std::string s;
+
+ public:
+  Base() : s("기반") { std::cout << "기반 클래스" << std::endl; }
+
+  void what() { std::cout << s << std::endl; }
+};
+
+class Derived : public Base {
+  std::string s;
+
+ public:
+  Derived() : s("파생"), Base() { std::cout << "파생 클래스" << std::endl; }
+
+  void what() { std::cout << s << std::endl; }
+};
+
+int main() {
+  Base p;
+  Derived c;
+
+  std::cout << "=== 포인터 버전 ===" << std::endl;
+  Derived* p_p = &p;
+  p_p->what();
+
+  return 0;
+}
